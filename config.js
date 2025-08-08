041a7185d22d8c4dcc72d0b61be6ae785699bcb2
Gimhanakaween30 <150015037+Gimhanakaween30@users.noreply.github.com>
2025-08-08 14:14:43 +0530

    Update config.js

--- config.js
+++ config.js
@@ -5,7 +5,7 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "vd9DhKKb#aJPrcZ9KL5urD0iv6N190SG7LFRWRPRGaSu3A_8X9fI", // ඔයාගෙ session id එක
+SESSION_ID: process.env.SESSION_ID || "WIckwLgJ#eaPmFUr8KwrpmOTbYU2tL_E4clzDXJSrA3XSJoE5TUE", // ඔයාගෙ session id එක
 MONGODB: process.env.MONGODB || "mongodb://mongo:OHgNbWOlOqVgYmEXGbkNJazHttqLJmWn@ballast.proxy.rlwy.net:57977",  //ඔයාගෙ mongoDb url එක
 };
  
